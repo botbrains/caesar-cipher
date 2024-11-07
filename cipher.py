ddf298c372036aa6da20c23443e116b3bb9ad83e
Donovan Roberts <129420545+botbrains@users.noreply.github.com>
2024-11-07 15:57:40 -0500

    Update cipher.py
    caesar cipher encryption code.

--- cipher.py
+++ cipher.py
@@ -1,1 +1,16 @@
-# add your code here
+message = input("Please enter a message: ")
+shift = 5
+cipher = ""
+#Iterate each letter in the message
+for char in message.upper():
+    if char.isalpha():
+      char_code = ord(char)
+      new_char_code = char_code + shift
+      if new_char_code > 90:
+         new_char_code -= 26
+      new_char = chr(new_char_code)
+      cipher += new_char
+    else:
+      cipher = cipher + char
+print("Your encrypted message is: ", cipher.lower())
+
