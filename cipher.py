d125ebbcd82cd990e54d9b5e5e7e0b0753526abb
Donovan <donovan.roberts27@gmail.com>
2024-11-07 18:25:13 -0500

    fixed formatting issues.

--- cipher.py
+++ cipher.py
@@ -4,13 +4,12 @@ cipher = ""
 #Iterate each letter in the message
 for char in message.upper():
     if char.isalpha():
-      char_code = ord(char)
-      new_char_code = char_code + shift
-      if new_char_code > 90:
-         new_char_code -= 26
-      new_char = chr(new_char_code)
-      cipher += new_char
+        char_code = ord(char)
+        new_char_code = char_code + shift
+        if new_char_code > 90:
+            new_char_code -= 26
+        new_char = chr(new_char_code)
+        cipher += new_char
     else:
-      cipher = cipher + char
+        cipher = cipher + char
 print("Your encrypted message is: ", cipher.lower())
-
